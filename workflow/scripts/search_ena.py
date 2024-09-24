c89b7c1ccdbd94d5c561e3779870d413f186e7bd
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-09-24 15:28:17 +0200

    Use POST with tsv response

--- workflow/scripts/search_ena.py
+++ workflow/scripts/search_ena.py
@@ -1,5 +1,6 @@
 import sys
-from typing import Mapping, List
+import csv
+from typing import Mapping, List, Iterator
 import json
 import requests
 import urllib.parse
@@ -10,11 +11,14 @@ import pandas as pd
 SEARCH_FIELDS_URL = "https://www.ebi.ac.uk/ena/portal/api/returnFields?dataPortal=ena&result=read_run&format=json"
 POST_URL = "https://www.ebi.ac.uk/ena/portal/api/search"
 
-HEADERS = {'content-type' : 'application/x-www-form-urlencoded'}
+HEADERS = {
+    "content-type" : "application/x-www-form-urlencoded",
+    "connection": "keep-alive"
+}
 REQUEST_DATA_TEMPLATE = {
     "limit": snakemake.params.limit,
     "result": "read_run",
-    "format": "json"
+    "format": "tsv"
 }
 
 
@@ -39,16 +43,10 @@ def build_query(min_date: str, max_date: str, tax_id: str, host: str) -> str:
     return " AND ".join(item for item in items if item)
 
 
-def search_GISAID(fields: List[str], min_date: str, max_date: str, tax_id: str, host: str) -> Mapping[str, Mapping[str, str]]:
-    data = REQUEST_DATA_TEMPLATE.copy()
-    query_txt = build_query(min_date, max_date, tax_id, host)
-    data["query"] = urllib.parse.quote(query_txt, safe="")
-    data["fields"] = ",".join(fields)
-    r = requests.post(POST_URL, data=data, headers=HEADERS)
-    if r.status_code == 200:            
-        return {"query": data, "results": [match for match in r.json()]}
-    else:
-        sys.exit(f"Request failed with code {r.status_code}: {r.text}")
+def write_iter(content: Iterator, path: str):
+    with open(path, "wb") as fw:
+        for chunk in content:
+            fw.write(chunk)
 
 
 if __name__ == "__main__":
@@ -59,25 +57,23 @@ if __name__ == "__main__":
 
     # Perform search
     print("Performing ENA search", flush=True)
-    search_results = search_GISAID(
-        fields,
+    data = REQUEST_DATA_TEMPLATE.copy()
+    query_txt = build_query(
         snakemake.params.start_date,
         snakemake.params.end_date,
         snakemake.params.taxonomy,
         snakemake.params.host_scientific_name
     )
+    data["query"] = urllib.parse.quote(query_txt, safe="")
+    data["fields"] = ",".join(fields)
+    with requests.post(POST_URL, data=data, headers=HEADERS, stream=True) as r:
+        if r.status_code == 200:
+            print("Writing ENA search results", flush=True)
+            write_iter(r.iter_content(chunk_size=snakemake.params.chunksize), snakemake.output.table)
+        else:
+            sys.exit(f"Request failed with code {r.status_code}: {r.text}")
 
     # Write query
     print("Saving query data", flush=True)
-    with open(snakemake.output[1], "w") as fw:
-        query = search_results["query"].copy()
-        query["query"] = urllib.parse.unquote(query["query"])
-        json.dump(query, fw, indent=2)
-
-    # Build results
-    print("Building search results", flush=True)
-    search = pd.DataFrame.from_records(search_results["results"])
-    
-    # Write search results
-    print("Saving search results", flush=True)
-    search.to_csv(snakemake.output[0], index=False, compression="gzip")
+    with open(snakemake.output.query, "w") as fw:
+        json.dump(data | {"query": query_txt}, fw, indent=2)
