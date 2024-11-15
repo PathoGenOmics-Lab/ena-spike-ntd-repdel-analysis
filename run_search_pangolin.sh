f7830ff58ecf9f371afea0788cb138cf5d125823
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-11-15 16:59:53 +0100

    Update search with pangolin launcher

--- run_search_pangolin.sh
+++ run_search_pangolin.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 #SBATCH --job-name sp_repdel
-#SBATCH --mem 16GB
+#SBATCH --mem-per-cpu 16GB
 #SBATCH --cpus-per-task 1
-#SBATCH --ntasks 1
+#SBATCH --ntasks 2
 #SBATCH --qos interactive
 #SBATCH --part interactive
 #SBATCH --time 1-00:00:00
 #SBATCH --output slurm-%x-%J.out
 
-srun snakemake --workflow-profile profiles/garnatxa --until summarize_ena_search
+NBATCHES=1000
 
-NBATCHES=50
-for i in $(seq 1 $NBATCHES); do
-    srun snakemake --keep-going --workflow-profile profiles/garnatxa --until summarize_ena_search_after_processing --batch all=$i/$NBATCHES
+if [ -f summarize_ena_search.done ]; then
+    echo ">>> SEARCHING"
+    srun snakemake --workflow-profile profiles/garnatxa --until summarize_ena_search
+    touch summarize_ena_search.done
+fi
+
+for i in $(seq 1 2 $NBATCHES); do
+    echo ">>> LAUNCHING BATCH $i"
+    srun -n1 snakemake --keep-going --workflow-profile profiles/garnatxa --until summarize_ena_search_after_processing --batch coverage=$i/$NBATCHES &
+    echo ">>> LAUNCHING BATCH $((i+1))"
+    srun -n1 snakemake --keep-going --workflow-profile profiles/garnatxa --until summarize_ena_search_after_processing --batch coverage=$((i+1))/$NBATCHES &
+    wait
 done
