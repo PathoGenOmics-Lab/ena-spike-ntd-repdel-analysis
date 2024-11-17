6e8dd6ba92ccd50574e8002eac4bc1f9a8e6907d
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-11-17 12:32:01 +0100

    Fix date filter

--- run_split_by_date.sh
+++ run_split_by_date.sh
@@ -7,7 +7,7 @@
 #SBATCH --time 3-00:00:00
 #SBATCH --output slurm-%x-%J.out
 
-N=1000
+N=1000  # approx
 
 echo ">>> RUNNING SEARCH"
 srun snakemake --workflow-profile profiles/garnatxa --until summarize_ena_search --config OUTPUT=output_search
@@ -15,7 +15,7 @@ srun snakemake --workflow-profile profiles/garnatxa --until summarize_ena_search
 echo ">>> SPLITTING DATES"
 srun python split_dates.py -n $N output_search/ena/search.filtered.tsv dates.tsv
 
-while read -r n1 start1 end1; do
+while read -r start end n; do
     echo ">>> RUNNING $n samples from $start to $end"
     srun snakemake --workflow-profile profiles/garnatxa --config OUTPUT=output_${n}_${start}_${end} START_DATE=$start END_DATE=$end
 done < dates.tsv
