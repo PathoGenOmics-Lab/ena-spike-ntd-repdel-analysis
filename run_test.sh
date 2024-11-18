c1b6f30ae145978858d56184d9379b4acb3ac833
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-11-18 18:18:06 +0100

    Update launcher

--- run_test.sh
+++ run_test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --job-name sp_repdel
+#SBATCH --job-name t_repdel
 #SBATCH --mem 16GB
 #SBATCH --cpus-per-task 1
 #SBATCH --ntasks 1
@@ -12,20 +12,18 @@ set -e
 NBATCHES=2
 LIMIT=1000
 
-if [ ! -f search.test.tsv ]; then
+if [ ! -f search.tsv ]; then
     echo ">>> SEARCHING"
-    python search_ena.py search.test.tsv \
+    python search_ena.py search.tsv \
         --start-date "2021-11-01" --end-date "2022-08-01" --limit $LIMIT
 fi
 
 for i in $(seq 1 $NBATCHES); do
     echo ">>> [1] START BATCH $i of $NBATCHES"
-    srun snakemake --config OUTPUT=output_test SEARCH_TABLE=search.test.tsv FILTERED_TABLE=search.test.filtered.tsv \
-        --workflow-profile profiles/garnatxa --config UNTIL_FILTER=True --batch consensus_merge=$i/$NBATCHES search.test.filtered.tsv
+    srun snakemake --workflow-profile profiles/garnatxa --config UNTIL_FILTER=True --batch consensus_merge=$i/$NBATCHES search.filtered.tsv
 done
 
 for i in $(seq 1 $NBATCHES); do
     echo ">>> [2] START BATCH $i of $NBATCHES"
-    srun snakemake --config OUTPUT=output_test SEARCH_TABLE=search.test.tsv FILTERED_TABLE=search.test.filtered.tsv \
-        --workflow-profile profiles/garnatxa --config UNTIL_FILTER=False --batch filter_haplotype=$i/$NBATCHES
+    srun snakemake --workflow-profile profiles/garnatxa --config UNTIL_FILTER=False --batch filter_haplotype=$i/$NBATCHES
 end
