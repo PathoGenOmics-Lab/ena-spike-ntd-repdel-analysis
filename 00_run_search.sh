935254f554ca8741d036ee6b8bb649552f22b0f0
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-12-12 18:34:26 +0100

    Do not recreate big search lists

--- 00_run_search.sh
+++ 00_run_search.sh
@@ -10,6 +10,10 @@
 set -e
 
 echo ">>> SEARCHING"
-srun python scripts/search_ena.py search.raw.complete.tsv --start-date "2021-11-01" --end-date "2022-08-01"
-srun python scripts/shuffle_search.py search.raw.complete.tsv search.shuffled.complete.tsv
+if [ ! -f search.raw.complete.tsv ]; then
+    srun python scripts/search_ena.py search.raw.complete.tsv --start-date "2021-11-01" --end-date "2022-08-01"
+fi
+if [ ! -f search.shuffled.complete.tsv ]; then
+    srun python scripts/shuffle_search.py search.raw.complete.tsv search.shuffled.complete.tsv
+fi
 srun python scripts/subsample.py search.shuffled.complete.tsv search.tsv --subsample 100000
