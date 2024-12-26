f3c1c81255be3d683d9957ead938f971b9ba3cd9
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-12-26 11:37:46 +0100

    Use SLURM multitasking

---
+++ 01c_run_haplotypes_array_chunked.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#SBATCH --job-name 01c-srepdel
+#SBATCH --mem-per-cpu 2GB
+#SBATCH --cpus-per-task 2
+#SBATCH --ntasks 16
+#SBATCH --qos short
+#SBATCH --time 1-00:00:00
+#SBATCH --output slurm-%x-%j.out
+
+CHUNK_SIZE=$SLURM_NTASKS
+i=$SLURM_ARRAY_TASK_ID
+
+TABLE="search.shuffled.filtered.tsv"
+N_CHUNKS=$(scripts/iter_samples.py --size $CHUNK_SIZE --count <$TABLE)
+MAX_CHUNK=$(( $N_CHUNKS - 1 ))
+
+head $TABLE >head.tsv  # unused within workflow, saves memory
+
+srun snakemake --conda-create-envs-only --config SEARCH_TABLE=head.tsv
+
+for chunk in $(seq 0 $MAX_CHUNK); do
+    echo "$(date) | >>> RUNNING FOR CHUNK $chunk OF $N_CHUNKS"
+    for sample in $(scripts/iter_samples.py --chunk $chunk --size $CHUNK_SIZE <$TABLE); do
+        echo "$(date) | >>> RUNNING FOR SAMPLE $sample"
+        srun --ntasks 1 -c $SLURM_CPUS_PER_TASK --mem-per-cpu $SLURM_MEM_PER_CPU snakemake \
+            "output/repdel/filter_haplotype/$sample/"{Rep_69_70,Rep_143_145,Rep_Both}".inclpct_"{95,75}".exclpct_"{5,25}".csv" \
+            --keep-going \
+            --workflow-profile profiles/default --cores $SLURM_CPUS_PER_TASK \
+            --config SEARCH_TABLE=head.tsv &
+    done
+    wait
+done
--- scripts/iter_samples.py
+++ scripts/iter_samples.py
@@ -4,6 +4,7 @@
 import sys
 import argparse
 import csv
+import itertools as it
 
 
 TEMPLATE = "{}/{}/{}/{}/{}_{}_{}"
@@ -23,15 +24,18 @@ def extract_fields(record: dict) -> list:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--chunk", type=int, required=True)  # from 0
-    parser.add_argument("--nrows", type=int, required=True)
-    parser.add_argument("--size", type=int, required=True)
+    parser.add_argument("--size", type=int, required=True, help="Number of items in chunk")
+    parser.add_argument("--chunk", type=int, default=0, help="Chunk index to return, starting from 0")
+    parser.add_argument("--count", action="store_true", help="Just count the number of chunks")
     args = parser.parse_args()
 
-    min_row = args.size * args.chunk
-    max_row = args.size * (args.chunk + 1)
-
     reader = csv.DictReader(sys.stdin, delimiter="\t")
-    for i, record in enumerate(reader):
-        if min_row <= i and i < max_row:
-            sys.stdout.write(TEMPLATE.format(*extract_fields(record)) + "\n")
+
+    if args.count:
+        n = sum(1 for _ in it.batched(reader, args.size))
+        sys.stdout.write(str(n))
+    else:
+        for i, records in enumerate(it.batched(reader, args.size)):
+            if i == args.chunk:
+                for record in records:
+                    sys.stdout.write(TEMPLATE.format(*extract_fields(record)) + "\n")
