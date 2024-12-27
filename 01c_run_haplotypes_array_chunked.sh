aced68cb4480e03bdff28df541428fdc3da7250a
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-12-27 12:59:45 +0100

    Update step arguments to remove clutter messages and avoid OOM errors

--- 01c_run_haplotypes_array_chunked.sh
+++ 01c_run_haplotypes_array_chunked.sh
@@ -15,13 +15,13 @@ N_CHUNKS=$(scripts/iter_samples.py --size $CHUNK_SIZE --count <$TABLE)
 MAX_CHUNK=$(( $N_CHUNKS - 1 ))
 
 head $TABLE >head.tsv  # unused within workflow, saves memory
-srun --ntasks 1 -c 2 snakemake --conda-create-envs-only --config SEARCH_TABLE=head.tsv
+srun --ntasks 1 -c $SLURM_CPUS_PER_TASK --mem-per-cpu $(( $SLURM_MEM_PER_CPU * $SLURM_NTASKS )) snakemake --conda-create-envs-only --config SEARCH_TABLE=head.tsv
 
 for chunk in $(seq 0 $MAX_CHUNK); do
     echo "$(date) | >>> RUNNING FOR CHUNK $chunk OF $N_CHUNKS"
     for sample in $(scripts/iter_samples.py --chunk $chunk --size $CHUNK_SIZE <$TABLE); do
         echo "$(date) | >>> RUNNING FOR SAMPLE $sample"
-        srun --ntasks 1 -c $SLURM_CPUS_PER_TASK --mem-per-cpu $SLURM_MEM_PER_CPU --output slurm-%x-%j_%s.out \
+        srun --nnodes 1 --ntasks 1 -c $SLURM_CPUS_PER_TASK --mem-per-cpu $SLURM_MEM_PER_CPU --output slurm-%x-%j_%s.out \
             snakemake \
             "output/repdel/filter_haplotype/$sample/"{Rep_69_70,Rep_143_145,Rep_Both}".inclpct_"{95,75}".exclpct_"{5,25}".csv" \
             --nolock \
