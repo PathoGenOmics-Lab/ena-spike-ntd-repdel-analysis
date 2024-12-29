1bb02a495720413833af55cb924d0979584e381f
Miguel Álvarez Herrera <miguel.alvarez@uv.es>
2024-12-29 20:39:00 +0100

    Update chunk generation to account for the SLURM max array size

--- 00b_generate_chunks.sh
+++ 00b_generate_chunks.sh
@@ -7,4 +7,24 @@
 #SBATCH --time 1-00:00:00
 #SBATCH --output slurm-%x-%J.out
 
-srun scripts/chunk_samples.py --size 16 chunks < search.shuffled.filtered.tsv
+N_PER_GROUP=$(( 5000 * 16 ))  # SLURM MaxArraySize * chunk size
+
+records=$(mktemp)
+groups_dir=$(mktemp -d)
+
+echo "$(date) | Extracting records"
+srun tail -n +2 search.shuffled.filtered.tsv >$records
+echo "$(date) | Splitting table"
+srun split -l $N_PER_GROUP -d $records $groups_dir/group_
+echo "$(date) | Removing records file"
+srun rm $records
+
+for group in $groups_dir/group_*; do
+    name=${group##*/}
+    echo "$(date) | Chunking $name"
+    srun scripts/chunk_samples.py --size 16 chunks/$name <search.shuffled.filtered.tsv
+done
+
+echo "$(date) | Removing groups directory"
+srun rm $groups_dir/*
+rmdir $groups_dir
